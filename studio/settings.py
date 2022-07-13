936f308140046c518ea24d9354068e637f339499
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-07-13 08:05:31 +0000

    agli studios api,added new server address

--- studio/settings.py
+++ studio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.aglistudiosgh.xyz', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.aglistudiosgh.xyz','143.110.151.230', '127.0.0.1', 'localhost']
 
 # Application definition
 
